1f762a3a4acc27be389b913a099435961461daa1
Lab <kennethmorales847@gmail.com>
2025-02-17 11:38:15 -0600

    Agregando clases

--- Lab6/Lab6.cpp
+++ Lab6/Lab6.cpp
@@ -2,6 +2,23 @@
 #include <iostream>
 using namespace std;
 
+//Clases
+
+class Vehiculo {
+private:
+    string marca;
+    string modelo;
+    double precio;
+
+public:
+    //Constructor
+    Vehiculo(string ma, string mo, double pr) {
+        marca = ma;
+        modelo = mo;
+        precio = pr;
+    }
+};
+
 int main()
 {
     std::cout << "Hello World!\n";
